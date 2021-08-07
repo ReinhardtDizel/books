3f652dc4c1eb3e2653bbf22f09cb98c07d3d11d8
Michael <dize882@gmail.com>
2021-08-07 17:47:14 +0300

    Начало разработки приложения
    Добавил компонет строки, столбца, заголовка таблицы

---
+++ src/BookRow.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BooksTable from "./App";
+
+interface Props {
+    // TODO: Здесь определить сворйства так же как и в
+};
+
+// TODO: Убрать "Remove" после переноса кода строки сюда
+class BookRowRemove extends React.Component<Props, {}> {
+
+}
+export default BookRowRemove;
---
+++ src/BooksTable.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import BookRowRemove from "./BookRow";
+
+// TODO: Убрать "Remove" после переноса кода таблицы сюда
+class BooksTableRemove extends React.Component {
+
+}
+export default BooksTableRemove;
